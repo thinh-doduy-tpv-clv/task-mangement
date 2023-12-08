1f25176e2adaac9fe98d0af0aa3c7eab9721c4bb
thinh-doduy-tpv-clv <thinh.doduy.tpv@one-line.com>
2023-12-08 14:26:15 +0700

    [DEV] update typeorm configuration

--- task_service/src/app.module.ts
+++ task_service/src/app.module.ts
@@ -1,16 +1,28 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { DATABASE_CONFIG } from './config/database.config';
 import { TasksModule } from './tasks/tasks.module';
 @Module({
   imports: [
     TasksModule,
-    TypeOrmModule.forRoot({
-      ...DATABASE_CONFIG,
-      entities: ['dist/**/*.entity.ts'],
-      synchronize: true,
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('POSTGRES_HOST'),
+        port: +configService.get('POSTGRES_PORT'),
+        username: configService.get('POSTGRES_USERNAME'),
+        password: configService.get('POSTGRES_PASSWORD'),
+        database: configService.get('POSTGRES_DB'),
+        entities: ['dist/**/*.entity.ts'],
+        synchronize: true,
+      }),
+      inject: [ConfigService],
+    }),
+    ConfigModule.forRoot({
+      isGlobal: true,
     }),
   ],
   controllers: [AppController],
