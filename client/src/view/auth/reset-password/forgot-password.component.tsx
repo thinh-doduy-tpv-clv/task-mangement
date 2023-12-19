da759c074493d86f602f190db95d187f19a1059e
thinh-doduy-tpv-clv <thinh.doduy.tpv@one-line.com>
2023-12-19 14:30:54 +0700

    [DEV] fix text reset password

--- client/src/view/auth/reset-password/forgot-password.component.tsx
+++ client/src/view/auth/reset-password/forgot-password.component.tsx
@@ -122,7 +122,7 @@ const ForgotPasswordComponent: React.FunctionComponent<Props> = (props) => {
               type="submit"
               className="w-full text-white bg-slate-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
             >
-              Reset passwod
+              Reset password
             </button>
           </form>
         </div>
